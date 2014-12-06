0ad48c88ccf73a6bbec5f3d099d80805e52492ed
Logan <beaverl@msoe.edu>
2014-12-05 23:51:59 -0600

    Created initial "level" and scripts
    Also imported sample assets

---
+++ Krunch/Assets/StairTriggerScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StairTriggerScript : MonoBehaviour {
+
+
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("At the stairs");
+
+	}
+}
