0f6fa915f32fc117612e2b258a41efa959311e9e
Logan <beaverl@msoe.edu>
2014-12-06 00:17:43 -0600

    Updated stairs in scene

--- Krunch/Assets/StairScript.cs
+++ Krunch/Assets/StairScript.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class StairScript : MonoBehaviour {
 
+	Transform top;
+	Transform bottom;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		top = GameObject.Find ("HighPosition").transform;
+		bottom = GameObject.Find ("LowPosition").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void UseStairs(bool downward) {
+		Debug.Log ("Going Up");
+		if (downward) {
+			Debug.Log (bottom);
+			Debug.Log (top);
+		} else {
+			Debug.Log (top);
+		}
 	}
 }
--- Krunch/Assets/StairTriggerScript.cs
+++ Krunch/Assets/StairTriggerScript.cs
@@ -3,10 +3,31 @@ using System.Collections;
 
 public class StairTriggerScript : MonoBehaviour {
 
+	public bool top;
 
+	bool ready;
 
-	void OnTriggerEnter(Collider other) {
-		Debug.Log ("At the stairs");
+	StairScript parent;
 
+	void Awake() {
+		parent = GetComponentInParent<StairScript> ();
+	}
+
+	void Update() {
+
+		if (ready && Input.GetKeyDown (KeyCode.F))
+			parent.UseStairs (top);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag("Player")) {
+			ready = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.CompareTag("Player")) {
+			ready = false;
+		}
 	}
 }
