6b9c157cfe2fe23f2f16c40968086fe6ff96dd37
Ian Mahuta <mahutai@msoe.edu>
2014-12-06 13:45:07 -0600

    Added new sprites

--- Krunch/Assets/Scripts/LootableScript.cs
+++ Krunch/Assets/Scripts/LootableScript.cs
@@ -3,46 +3,39 @@ using System.Collections;
 
 public class LootableScript : MonoBehaviour {
 
-	public float lootTime = 2.5f; //time to loot this object
-	public GameObject[] contained;	//contained objects
-
-	public PlayerInventoryScript player; //the player's inventory
-
-	float cooldown; //time left looting
-
-	bool ready; //can this container be looted?
-	bool looting; //are we currently looting?
-	
+	bool ready; 
+	public PlayerInventoryScript player;
+	public float lootTime = 2.5f;
+	public GameObject[] contained;
+	float cooldown;
+	bool looting;
 
 	// Update is called once per frame
 	void Update () {
-		looting = looting && ready; //cancel looting if the player leaves the loot zone
-		if (ready && Input.GetKeyUp (KeyCode.F) && !looting) { //if the player presses action, begin looting
+		looting = looting && ready;
+		if (ready && Input.GetKeyUp (KeyCode.F) && !looting) {
 			looting = true;
 			cooldown = lootTime;
-			//pause player
-			//start loop animation
 		}
-		//loot progress
 		if (looting) {
 			cooldown -= Time.deltaTime;
-			if (cooldown <= 0) { //loot success, drop all held items into player inventory
+			if(cooldown <= 0){
 				looting = false;
-				//play idle player animation
-				//reenable player conrol
-				player.GiveItem (contained);
+				// play idle animation
+				// re-enable player control
+				player.GiveItem(contained);
 				contained = new GameObject[0];
 			}
 		}
 	}
 
-	void OnTriggerEnter2D(Collider2D other) {
-		if (other.CompareTag ("Player"))
-			ready = true;
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.CompareTag (Tags.Player))
+						ready = true;
 	}
 
-	void OnTriggerExit2D(Collider2D other) {
-		if (other.CompareTag ("Player"))
+	void OnTriggerExit2D (Collider2D other){
+		if (other.CompareTag (Tags.Player))
 			ready = false;
 	}
 }
