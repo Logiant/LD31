2790e73bfdd98356d098c41ac58aa61f1d113823
Logan <beaverl@msoe.edu>
2014-12-05 23:58:51 -0600

    Deleted Bloatware
    Freed from the garbage assits!

--- Krunch/Assets/Sample Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Krunch/Assets/Sample Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -14,24 +14,14 @@ public class Platformer2DUserControl : MonoBehaviour
 
     void Update ()
     {
-        // Read the jump input in Update so button presses aren't missed.
-#if CROSS_PLATFORM_INPUT
-        if (CrossPlatformInput.GetButtonDown("Jump")) jump = true;
-#else
 		if (Input.GetButtonDown("Jump")) jump = true;
-#endif
-
     }
 
 	void FixedUpdate()
 	{
 		// Read the inputs.
 		bool crouch = Input.GetKey(KeyCode.LeftControl);
-		#if CROSS_PLATFORM_INPUT
-		float h = CrossPlatformInput.GetAxis("Horizontal");
-		#else
 		float h = Input.GetAxis("Horizontal");
-		#endif
 
 		// Pass all parameters to the character control script.
 		character.Move( h, crouch , jump );
