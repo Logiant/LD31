fe7d7c8ba96fe84bccb9d59f9d53795a1dd7a616
Logiant <beaverl@msoe.edi>
2014-12-06 14:36:29 -0600

    Created floor prefabs
    The fade in and out!

---
+++ Krunch/Assets/RevealFloorScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RevealFloorScript : MonoBehaviour {
+
+	SpriteRenderer render;
+	bool visible;
+
+	void Awake() {
+		render = GetComponent<SpriteRenderer> ();
+	}
+
+	void Update() {
+		if (visible) {
+			render.color = new Vector4 (render.color.r, render.color.g, render.color.b, render.color.a - 0.1f);
+			if (render.color.a <= 0.1)
+				Destroy(this.gameObject);
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log (other.tag);
+		if (other.CompareTag(Tags.Player))
+			visible = true;
+	}
+}
