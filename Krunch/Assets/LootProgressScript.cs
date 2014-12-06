4d7beab63dfdc5533e2dac39f7fda1b74a695bbb
Logiant <beaverl@msoe.edi>
2014-12-06 15:04:36 -0600

    Finished loot progress behavior
    Loot!

--- Krunch/Assets/LootProgressScript.cs
+++ Krunch/Assets/LootProgressScript.cs
@@ -6,10 +6,33 @@ public class LootProgressScript : MonoBehaviour {
 	GameObject background1;
 	GameObject background2;
 
+	float maxTime;
+	float time;
+
 	void Awake() {
-		rotForeground = this.transform.Find ("Foreground 1");
+		rotForeground = GameObject.Find ("Foreground 1").transform;
 		background1 = GameObject.Find ("Background 1");
 		background2 = GameObject.Find ("Background 2");
 	}
 
+	void Update() {
+		if (time > 0) {
+			time -= Time.deltaTime;
+			float angle = (time / maxTime) * 360;
+			rotForeground.transform.rotation = Quaternion.Euler (0, 0, angle);
+			if (time <= maxTime/2) {
+				background1.SetActive(false);
+				background2.SetActive(false);
+			}
+		} else {
+			this.gameObject.SetActive (false);
+		}
+	}
+	public void Loot(float dt) {
+		maxTime = dt;
+		time = maxTime;
+		this.gameObject.SetActive (true);
+		background1.SetActive (true);
+		background2.SetActive (true);
+	}
 }
