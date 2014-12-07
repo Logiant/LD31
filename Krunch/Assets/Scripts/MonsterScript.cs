d07894476aec5c8179e689e02c631cd230f0090c
Logan <beaverl@msoe.edu>
2014-12-07 03:30:27 -0600

    Monster == Creepy Stalker
    Updated the MonsterScript to cause the monster to randomly search floors
    for you
    If he sees you, he will watch you and say PUNCH

--- Krunch/Assets/Scripts/MonsterScript.cs
+++ Krunch/Assets/Scripts/MonsterScript.cs
@@ -3,15 +3,70 @@ using System.Collections;
 
 public class MonsterScript : MonoBehaviour {
 	
-	public Transform player;
-	public float speed = 5;	
+	public float speed = 1;	
+	public Transform[] floorPositions;
+	public float stalkTime = 5f; //time the monster waits if you disappear
+
+	float cooldown;
 
 	Vector3 desiredPosition;
+	Transform eyePos;
+	float lastPlayerHeight;
 
+
+	void Awake() {
+		desiredPosition = transform.position;
+		eyePos = transform.FindChild ("Eyesight");
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		desiredPosition = new Vector3(transform.position.x, player.position.y - 5f, transform.position.z);
-		transform.position = Vector3.Lerp (transform.position, desiredPosition, speed*Time.deltaTime);
+		//raycast for player!
+		RaycastHit2D hit;
+		hit = Physics2D.Raycast (new Vector2(eyePos.position.x, eyePos.position.y), new Vector2 (-1, 0), Mathf.Infinity);
+		if (hit.collider != null && hit.collider.CompareTag(Tags.Player)) {
+			lastPlayerHeight = transform.position.y;
+			cooldown = stalkTime;
+		}
+		//if we are at our destination, chose a new course of action
+		Vector3 distance = desiredPosition - transform.position;
+		if (distance.sqrMagnitude <= 0.1) { //if we are at our target
+			PickTarget();
+			distance = desiredPosition - transform.position;
+		}
+		float vel = Mathf.Min (distance.magnitude * Time.deltaTime, speed * Time.deltaTime);
+		transform.position += (vel * distance.normalized);
+
+		//check if we are staring at the player
+		if (Mathf.Abs (transform.position.y - lastPlayerHeight) < 0.1f && hit.collider != null && hit.collider.CompareTag(Tags.Player)) { 
+			//PUNCH!
+			Debug.Log ("PUNCH");
+		}
+	}
+
+	void PickTarget() {
+		//if we know where the player is, move to them
+		if (lastPlayerHeight != 0) {
+			MoveToPlayer ();
+			if (Mathf.Abs (transform.position.y - lastPlayerHeight) < 0.1f) { 
+				//if we are where the player last was and don't see them, wait a few seconds
+				cooldown -= Time.deltaTime;
+				if (cooldown <= 0) { //forget where the player was
+					lastPlayerHeight = 0;
+				}
+			}
+		}
+		//randomly move between floors
+		else if (floorPositions.Length > 0)
+			RandomMovement ();
+	}
+
+	void RandomMovement() {
+		int i = Random.Range (0, floorPositions.Length); //dont go to the bottom floor
+		desiredPosition = new Vector3 (transform.position.x, floorPositions[i].position.y - 4.5f, transform.position.z);
+	}
+
+	void MoveToPlayer() { //placeholder method
+		desiredPosition = new Vector3(transform.position.x, lastPlayerHeight, transform.position.z);
 	}
 }
