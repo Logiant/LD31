72536c74ad368b6e1c76957cd34ca7260a41202e
Ian Mahuta <mahutai@msoe.edu>
2014-12-06 23:35:32 -0600

    Updated Hide and straightened scene
    Wrote the Update method in the HideScript and adjusted placing of
    objects in the Scene.

--- Krunch/Assets/Scripts/HideScript.cs
+++ Krunch/Assets/Scripts/HideScript.cs
@@ -7,21 +7,35 @@ public class HideScript : MonoBehaviour {
 	public GameObject player; // will become player controller once written
 	// Use this for initialization
 	void Start () {
-	
+	//potentially things with player
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (ready && Input.GetKeyUp (KeyCode.E) && !hiding) { // player can hide, is not hiding and presses to hide
+			hiding = true;
+			Debug.Log ("Hiding");
+			// set player state to hiding
+		}
+		else if (hiding && Input.GetKeyUp (KeyCode.E)) { // player is hiding and presses to leave hide
+			hiding = false;
+			Debug.Log ("Stop Hiding");
+			// set player to not hiding
+		}
+		else if (hiding && !ready){ // player is hiding and leaves hide area
+			hiding = false;
+			Debug.Log ("stop hiding, left area");
+			// set player to state not hiding
+		}
 	}
 
 	void OnTriggerEnter2D (Collider2D other){
-		if (other.CompareTag (Tags.Player))
+		if (other.CompareTag (Tags.Player)) // if player enters hide area toggle ready to true
 			ready = true;
 	}
 	
 	void OnTriggerExit2D (Collider2D other){
-		if (other.CompareTag (Tags.Player))
+		if (other.CompareTag (Tags.Player)) // if player leave hide area toggle ready to false
 			ready = false;
 	}
 }
