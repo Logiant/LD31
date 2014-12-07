2988ce6b68f16907abb557f3add7b04236e87ec9
Ian Mahuta <mahutai@msoe.edu>
2014-12-06 21:37:03 -0600

    Set up hide script.
    Added colliders and shit ... also made a basic script.

---
+++ Krunch/Assets/Scripts/HideScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideScript : MonoBehaviour {
+	bool ready; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.CompareTag (Tags.Player))
+			ready = true;
+	}
+	
+	void OnTriggerExit2D (Collider2D other){
+		if (other.CompareTag (Tags.Player))
+			ready = false;
+	}
+}
