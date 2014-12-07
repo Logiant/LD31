023183ad8e907a064008d8c0e48e4a04c0a861ea
Ian Mahuta <mahutai@msoe.edu>
2014-12-07 00:33:12 -0600

    SNOWMAN!
    ... and shit

--- Krunch/Assets/Scripts/MonsterScript.cs
+++ Krunch/Assets/Scripts/MonsterScript.cs
@@ -6,13 +6,12 @@ public class MonsterScript : MonoBehaviour {
 	public Transform player;
 	public float speed = 5;	
 
-	public Transform head;
 	Vector3 desiredPosition;
 
 	
 	// Update is called once per frame
 	void Update () {
-		desiredPosition = new Vector3(transform.position.x, player.position.y - 3.5f, transform.position.z);
+		desiredPosition = new Vector3(transform.position.x, player.position.y - 5f, transform.position.z);
 		transform.position = Vector3.Lerp (transform.position, desiredPosition, speed*Time.deltaTime);
 	}
 }
